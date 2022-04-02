fcaaa480e86a7341d6a963ca560505397a3cae2a
Waqar Malik <wmalloc@users.noreply.github.com>
2022-04-02 11:43:38 -0700

    Made teh data task a generic download type (#18)
    * Added mock and formated text (#10)
    
    formatted the files and linted
    
    made the methods public
    
    * Print Error message
    
    * Fixed the Request update methods (#12)
    
    * Fixed the compile errors (#14)
    
    * Make more data setters optional
    
    * Added some guard and made them optionals
    
    * Clean up document
    
    * Fixed data requests

--- Sources/Core/Extensions/WebService+Convertable.swift
+++ Sources/Core/Extensions/WebService+Convertable.swift
@@ -8,63 +8,57 @@
 import Foundation
 
 public extension WebService {
-	func dataTask(urlRequest: URLRequest, completion: WebService.DataHandler?) -> URLSessionDataTask? {
-		let dataTask = session.dataTask(with: urlRequest) { data, urlResponse, error in
-			if let error = error {
-				completion?(.failure(error))
-				return
-			}
-			guard let data = data, let urlResponse = urlResponse else {
-				completion?(.failure(URLError(.badServerResponse)))
-				return
-			}
-			do {
-				let validData = try data.ws_validate(urlResponse).ws_validateNotEmptyData()
-				completion?(.success(validData))
-			} catch {
-				completion?(.failure(error))
-			}
-		}
-		dataTask.resume()
-		return dataTask
-	}
+    func dataTask<T>(request: Request, mapper: @escaping DataMapper<Data, T>, completion: WebService.DataHandler<T>?) -> URLSessionDataTask? {
+        guard let urlRequest = try? request.urlRequest() else {
+            completion?(.failure(URLError(.badURL)))
+            return nil
+        }
+        return dataTask(urlRequest: urlRequest, mapper: mapper, completion: completion)
+    }
+    
+    func dataTask<T>(urlRequest: URLRequest, mapper: @escaping DataMapper<Data, T>, completion: WebService.DataHandler<T>?) -> URLSessionDataTask? {
+        let dataTask = session.dataTask(with: urlRequest) { data, urlResponse, error in
+            if let error = error {
+                completion?(.failure(error))
+                return
+            }
+            guard let data = data, let urlResponse = urlResponse else {
+                completion?(.failure(URLError(.badServerResponse)))
+                return
+            }
+            do {
+                let validData = try data.ws_validate(urlResponse).ws_validateNotEmptyData()
+                let mapped = try mapper(validData)
+                completion?(.success(mapped))
+            } catch {
+                completion?(.failure(error))
+            }
+        }
+        dataTask.resume()
+        return dataTask
+   }
 
-	func dataTask(request: Request, completion: WebService.DataHandler?) -> URLSessionDataTask? {
-		guard let urlRequest = try? request.urlRequest() else {
-			return nil
-		}
-		return dataTask(urlRequest: urlRequest, completion: completion)
+    func dataTask(urlRequest: URLRequest, completion: WebService.DataHandler<Data>?) -> URLSessionDataTask? {
+        dataTask(urlRequest: urlRequest, mapper: {$0}, completion: completion)
+    }
+
+	func dataTask(request: Request, completion: WebService.DataHandler<Data>?) -> URLSessionDataTask? {
+        dataTask(request: request, mapper: {$0}, completion: completion)
 	}
 
 	func decodableTask<T: Decodable>(request: Request, decoder: JSONDecoder = JSONDecoder(), completion: WebService.DecodeblHandler<T>?) -> URLSessionDataTask? {
-		dataTask(request: request) { result in
-			switch result {
-			case .failure(let error):
-				completion?(.failure(error))
-			case .success(let data):
-				do {
-					let decoded = try decoder.decode(T.self, from: data)
-					completion?(.success(decoded))
-				} catch {
-					completion?(.failure(error))
-				}
-			}
-		}
+        dataTask(request: request) { data in
+            try decoder.decode(T.self, from: data)
+        } completion: { result in
+            completion?(result)
+        }
 	}
 
 	func serializableTask(request: Request, options: JSONSerialization.ReadingOptions = .allowFragments, completion: WebService.SerializableHandler?) -> URLSessionDataTask? {
-		dataTask(request: request) { result in
-			switch result {
-			case .failure(let error):
-				completion?(.failure(error))
-			case .success(let data):
-				do {
-					let decoded = try JSONSerialization.jsonObject(with: data, options: options)
-					completion?(.success(decoded))
-				} catch {
-					completion?(.failure(error))
-				}
-			}
-		}
+        dataTask(request: request) { data in
+            try JSONSerialization.jsonObject(with: data, options: options)
+        } completion: { result in
+            completion?(result)
+        }
 	}
 }
