fed16d372c527cd1040c94c5c8df4e79edc74eaa
Waqar Malik <wmalloc@users.noreply.github.com>
2022-04-17 00:17:58 -0700

    Feature/remove service provider (#22)
    * Cleanup the ServicePublisher
    
    * Added data publisher

--- Sources/Concurrency/WebService+Concurrency.swift
+++ Sources/Concurrency/WebService+Concurrency.swift
@@ -5,13 +5,23 @@
 //  Created by Waqar Malik on 6/25/21.
 //
 
-import Combine
 import Foundation
 import WebService
+import WebServiceCombine
 
 @available(macOS 10.15, iOS 13, tvOS 13, macCatalyst 13, watchOS 6, *)
 public extension WebService {
-    func data(request: URLRequest) async throws -> (data: Data, response: URLResponse) {
+    func data(from url: URL) async throws -> (data: Data, response: URLResponse) {
+        let dataResponse: (data: Data, response: URLResponse)
+        if #available(macOS 12, iOS 15, tvOS 15, macCatalyst 15, watchOS 8, *) {
+            dataResponse = try await session.data(from: url, delegate: nil)
+        } else {
+            dataResponse = try await session.data(from: url)
+        }
+        return dataResponse
+    }
+
+    func data(for request: URLRequest) async throws -> (data: Data, response: URLResponse) {
         let dataResponse: (data: Data, response: URLResponse)
         if #available(macOS 12, iOS 15, tvOS 15, macCatalyst 15, watchOS 8, *) {
             dataResponse = try await session.data(for: request, delegate: nil)
@@ -21,21 +31,22 @@ public extension WebService {
         return dataResponse
     }
 
-	func data<ObjectType>(request: URLRequest, transform: DataMapper<Data, ObjectType>) async throws -> ObjectType {
-		let (data, response) = try await data(request: request)
-		try response.ws_validate()
-		return try transform(data)
+	func data<ObjectType>(for request: URLRequest, transform: DataMapper<(data: Data, response: URLResponse), ObjectType>) async throws -> ObjectType {
+		let result = try await data(for: request)
+		return try transform(result)
 	}
 
 	func decodable<ObjectType: Decodable>(request: URLRequest, decoder: JSONDecoder = JSONDecoder()) async throws -> ObjectType {
-		try await data(request: request) { data in
-			try decoder.decode(ObjectType.self, from: data)
+		try await data(for: request) { result in
+            let data = try result.data.ws_validateNotEmptyData()
+            return try decoder.decode(ObjectType.self, from: data)
 		}
 	}
 
-	func serializable(request: URLRequest, options: JSONSerialization.ReadingOptions = .allowFragments) async throws -> Any {
-		try await data(request: request) { data in
-			try JSONSerialization.jsonObject(with: data, options: options)
+	func serializable(for request: URLRequest, options: JSONSerialization.ReadingOptions = .allowFragments) async throws -> Any {
+		try await data(for: request) { result in
+            let data = try result.data.ws_validateNotEmptyData()
+            return try JSONSerialization.jsonObject(with: data, options: options)
 		}
 	}
 }
