bc7276f97bccca0ec77507fe266d0c2edb61a393
Waqar Malik <wmalloc@users.noreply.github.com>
2022-10-05 14:50:45 -0700

    Cleanup the dataTask

--- Sources/Concurrency/URLSession+Concurrency.swift
+++ Sources/Concurrency/URLSession+Concurrency.swift
@@ -15,28 +15,26 @@ import Foundation
 @available(macOS, introduced: 10.15, deprecated: 12.0, message: "Use the built-in API instead")
 public extension URLSession {
 	func data(from url: URL) async throws -> (Data, URLResponse) {
-		try await withCheckedThrowingContinuation { continuation in
-			let task = self.dataTask(with: url) { data, response, error in
-				guard let data = data, let response = response else {
-					let error = error ?? URLError(.badServerResponse)
-					return continuation.resume(throwing: error)
-				}
-				continuation.resume(returning: (data, response))
-			}
-			task.resume()
-		}
-	}
+        try await data(for: URLRequest(url: url))
+    }
 
 	func data(for request: URLRequest) async throws -> (Data, URLResponse) {
-		try await withCheckedThrowingContinuation { continuation in
-			let task = self.dataTask(with: request) { data, response, error in
-				guard let data = data, let response = response else {
-					let error = error ?? URLError(.badServerResponse)
-					return continuation.resume(throwing: error)
-				}
-				continuation.resume(returning: (data, response))
-			}
-			task.resume()
-		}
+        var dataTask: URLSessionDataTask?
+        let onCancel = { dataTask?.cancel() }
+        
+        return try await withTaskCancellationHandler(handler: {
+            onCancel()
+        }, operation: {
+            try await withCheckedThrowingContinuation { continuation in
+                dataTask = self.dataTask(with: request) { data, response, error in
+                    guard let data = data, let response = response else {
+                        let error = error ?? URLError(.badServerResponse)
+                        return continuation.resume(throwing: error)
+                    }
+                    continuation.resume(returning: (data, response))
+                }
+                dataTask?.resume()
+            }
+        })
 	}
 }
