d436265421d8ed1b11ddfb353499f52bce6a7ea0
Waqar Malik <wmalloc@users.noreply.github.com>
2022-10-08 11:05:46 -0700

    Fixed  cancelation task

--- Sources/Concurrency/URLSession+Concurrency.swift
+++ Sources/Concurrency/URLSession+Concurrency.swift
@@ -22,19 +22,20 @@ public extension URLSession {
         var dataTask: URLSessionDataTask?
         let onCancel = { dataTask?.cancel() }
         
-        return try await withTaskCancellationHandler(handler: {
-            onCancel()
-        }, operation: {
-            try await withCheckedThrowingContinuation { continuation in
-                dataTask = self.dataTask(with: request) { data, response, error in
+        return try await withTaskCancellationHandler(operation: {
+            try await withCheckedThrowingContinuation({ continuation -> Void in
+                dataTask = self.dataTask(with: request, completionHandler: { data, response, error in
                     guard let data = data, let response = response else {
                         let error = error ?? URLError(.badServerResponse)
                         return continuation.resume(throwing: error)
                     }
                     continuation.resume(returning: (data, response))
-                }
+                })
+                
                 dataTask?.resume()
-            }
+            })
+        }, onCancel: {
+            onCancel()
         })
-	}
+    }
 }
