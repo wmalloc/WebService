06822529ed3f6285c51dc7ab8ea35f87cdc5b187
Waqar Malik <wmalloc@users.noreply.github.com>
2022-10-10 23:35:45 -0700

    Formatting

--- Sources/Concurrency/URLSession+Concurrency.swift
+++ Sources/Concurrency/URLSession+Concurrency.swift
@@ -15,27 +15,27 @@ import Foundation
 @available(macOS, introduced: 10.15, deprecated: 12.0, message: "Use the built-in API instead")
 public extension URLSession {
 	func data(from url: URL) async throws -> (Data, URLResponse) {
-        try await data(for: URLRequest(url: url))
-    }
+		try await data(for: URLRequest(url: url))
+	}
 
 	func data(for request: URLRequest) async throws -> (Data, URLResponse) {
-        var dataTask: URLSessionDataTask?
-        let onCancel = { dataTask?.cancel() }
-        
-        return try await withTaskCancellationHandler(operation: {
-            try await withCheckedThrowingContinuation({ continuation -> Void in
-                dataTask = self.dataTask(with: request, completionHandler: { data, response, error in
-                    guard let data = data, let response = response else {
-                        let error = error ?? URLError(.badServerResponse)
-                        return continuation.resume(throwing: error)
-                    }
-                    continuation.resume(returning: (data, response))
-                })
-                
-                dataTask?.resume()
-            })
-        }, onCancel: {
-            onCancel()
-        })
-    }
+		var dataTask: URLSessionDataTask?
+		let onCancel = { dataTask?.cancel() }
+
+		return try await withTaskCancellationHandler(operation: {
+			try await withCheckedThrowingContinuation { continuation in
+				dataTask = self.dataTask(with: request, completionHandler: { data, response, error in
+					guard let data = data, let response = response else {
+						let error = error ?? URLError(.badServerResponse)
+						return continuation.resume(throwing: error)
+					}
+					continuation.resume(returning: (data, response))
+				})
+
+				dataTask?.resume()
+			}
+		}, onCancel: {
+			onCancel()
+		})
+	}
 }
