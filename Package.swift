0e21d044b223b57cd4c158467841187ec5aa398e
Waqar Malik <wmalloc@users.noreply.github.com>
2023-05-10 21:23:20 -0700

    Fixed spelling mistake (#48)

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         .library(name: "WebServiceURLMock", targets: ["WebServiceURLMock"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/wmalloc/URLRequestable.git", .upToNextMajor(from: "0.0.5")),
+        .package(url: "https://github.com/wmalloc/URLRequestable.git", .upToNextMajor(from: "0.0.7")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
