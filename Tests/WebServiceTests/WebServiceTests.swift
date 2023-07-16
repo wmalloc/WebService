7217143cee4bd867693f71eecea26e558bc1b97a
Waqar Malik <wmalloc@users.noreply.github.com>
2023-07-15 17:48:52 -0700

    Fixed Tests

--- Tests/WebServiceTests/WebServiceTests.swift
+++ Tests/WebServiceTests/WebServiceTests.swift
@@ -7,196 +7,232 @@
 
 import Combine
 import Foundation
+import HTTPTypes
 import os.log
+import URLRequestable
 @testable import WebService
 @testable import WebServiceCombine
 @testable import WebServiceConcurrency
 @testable import WebServiceURLMock
-import HTTPTypes
-import URLRequestable
-			.setMethod(.get)
-		let requestURL = request.url
-		XCTAssertNotNil(requestURL)
-		URLProtocolMock.requestHandlers[requestURL!] = { request in
-			guard let url = request.url, url == requestURL else {
-				throw URLError(.badURL)
-			}
+import XCTest
 
-			return (Response.valid, Data())
-		}
+final class WebServiceTests: XCTestCase {
+    static let baseURLString = "http://localhost:8080"
+    static let baseURL = URL(string: "http://localhost:8080")!
 
-		let publisher = webService.dataPublisher(for: request)
-		let validTest = evalValidResponseTest(publisher: publisher)
-		wait(for: validTest.expectations, timeout: testTimeout)
-		validTest.cancellable?.cancel()
-	}
+    static var allTests = [("testInvalidResponse", testInvalidResponse), ("testValidDataResponse", testValidDataResponse), ("testNetworkFailure", testNetworkFailure),
+                           ("testAsync", testAsync), ("testAsyncDecodable", testAsyncDecodable), ("testAsyncSerializable", testAsyncSerializable)]
 
-	func testInvalidResponse() throws {
-		let request = URLRequest(url: Self.baseURL)
-			.setMethod(.get)
-		let requestURL = request.url
-		XCTAssertNotNil(requestURL)
-		URLProtocolMock.requestHandlers[requestURL!] = { request in
-			guard let url = request.url, url == requestURL else {
-				throw URLError(.badURL)
-			}
+    let testTimeout: TimeInterval = 1
+    var webService: WebService!
 
-			return (Response.invalid401, Data())
-		}
+    enum Response {
+        static let invalid = URLResponse(url: WebServiceTests.baseURL, mimeType: nil, expectedContentLength: 0, textEncodingName: nil)
+        static let valid = HTTPURLResponse(url: WebServiceTests.baseURL, statusCode: 200, httpVersion: nil, headerFields: nil)!
+        static let invalid300 = HTTPURLResponse(url: WebServiceTests.baseURL, statusCode: 300, httpVersion: nil, headerFields: nil)!
+        static let invalid401 = HTTPURLResponse(url: WebServiceTests.baseURL, statusCode: 401, httpVersion: nil, headerFields: nil)!
+    }
 
-		let publisher = webService.dataPublisher(for: request)
-		let invalidTest = evalInvalidResponseTest(publisher: publisher)
-		wait(for: invalidTest.expectations, timeout: testTimeout)
-		invalidTest.cancellable?.cancel()
-	}
+    let networkError = NSError(domain: "NSURLErrorDomain", code: -1004, /* kCFURLErrorCannotConnectToHost*/ userInfo: nil)
 
-	func testValidDataResponse() throws {
-		let request = URLRequest(url: Self.baseURL)
-			.setMethod(.get)
-		let requestURL = request.url
-		XCTAssertNotNil(requestURL)
-		URLProtocolMock.requestHandlers[requestURL!] = { request in
-			guard let url = request.url, url == requestURL else {
-				throw URLError(.badURL)
-			}
+    override func setUpWithError() throws {
+        let config = URLSessionConfiguration.ephemeral
+        config.protocolClasses = [URLProtocolMock.self]
 
-			let data = Data("{}".utf8)
-			return (Response.valid, data)
-		}
+        // and create the URLSession from that
+        let session = URLSession(configuration: config)
+        webService = WebService(session: session)
+    }
 
-		let publisher = webService.dataPublisher(for: request)
-		let invalidTest = evalValidResponseTest(publisher: publisher)
-		wait(for: invalidTest.expectations, timeout: testTimeout)
-		invalidTest.cancellable?.cancel()
-	}
+    override func tearDownWithError() throws {
+        webService = nil
+    }
 
-	func testNetworkFailure() throws {
-		let request = URLRequest(url: Self.baseURL)
-			.setMethod(.get)
-		let requestURL = request.url
-		XCTAssertNotNil(requestURL)
-		URLProtocolMock.requestHandlers[requestURL!] = { request in
-			guard let url = request.url, url == requestURL else {
-				throw URLError(.badURL)
-			}
+    func testValidResponse() throws {
+        let request = URLRequest(url: Self.baseURL)
+            .setMethod(.get)
+        let requestURL = request.url
+        XCTAssertNotNil(requestURL)
+        URLProtocolMock.requestHandlers[requestURL!] = { request in
+            guard let url = request.url, url == requestURL else {
+                throw URLError(.badURL)
+            }
 
-			let data = Data("{}".utf8)
-			return (Response.invalid401, data)
-		}
-		let publisher = webService.dataPublisher(for: request)
-		let invalidTest = evalInvalidResponseTest(publisher: publisher)
-		wait(for: invalidTest.expectations, timeout: testTimeout)
-		invalidTest.cancellable?.cancel()
-	}
+            return (Response.valid, Data())
+        }
 
-	func evalValidResponseTest<P: Publisher>(publisher: P?) -> (expectations: [XCTestExpectation], cancellable: AnyCancellable?) {
-		XCTAssertNotNil(publisher)
+        let publisher = webService.dataPublisher(for: request)
+        let validTest = evalValidResponseTest(publisher: publisher)
+        wait(for: validTest.expectations, timeout: testTimeout)
+        validTest.cancellable?.cancel()
+    }
 
-		let finished = expectation(description: "Finished")
+    func testInvalidResponse() throws {
+        let request = URLRequest(url: Self.baseURL)
+            .setMethod(.get)
+        let requestURL = request.url
+        XCTAssertNotNil(requestURL)
+        URLProtocolMock.requestHandlers[requestURL!] = { request in
+            guard let url = request.url, url == requestURL else {
+                throw URLError(.badURL)
+            }
 
-		let cancellable = publisher?.sink(receiveCompletion: { completion in
-			if case .failure(let error) = completion {
-				os_log(.error, log: OSLog.tests, "TEST ERROR %@", error.localizedDescription)
-			}
-			finished.fulfill()
-		}, receiveValue: { response in
-			XCTAssertNotNil(response)
-			os_log(.info, log: OSLog.tests, "%@", "\(response)")
-		})
-		return (expectations: [finished], cancellable: cancellable)
-	}
+            return (Response.invalid401, Data())
+        }
 
-	func evalInvalidResponseTest<P: Publisher>(publisher: P?) -> (expectations: [XCTestExpectation], cancellable: AnyCancellable?) {
-		XCTAssertNotNil(publisher)
+        let publisher = webService.dataPublisher(for: request)
+        let invalidTest = evalInvalidResponseTest(publisher: publisher)
+        wait(for: invalidTest.expectations, timeout: testTimeout)
+        invalidTest.cancellable?.cancel()
+    }
 
-		let finished = expectation(description: "Finsihed")
+    func testValidDataResponse() throws {
+        let request = URLRequest(url: Self.baseURL)
+            .setMethod(.get)
+        let requestURL = request.url
+        XCTAssertNotNil(requestURL)
+        URLProtocolMock.requestHandlers[requestURL!] = { request in
+            guard let url = request.url, url == requestURL else {
+                throw URLError(.badURL)
+            }
 
-		let cancellable = publisher?.sink(receiveCompletion: { completion in
-			switch completion {
-			case .failure(let error):
-				os_log(.error, log: OSLog.tests, "TEST FULFILLED %@", error.localizedDescription)
-			case .finished:
-				XCTFail("Result should be error")
-			}
-			finished.fulfill()
-		}, receiveValue: { response in
-			XCTAssertNotNil(response)
-			os_log(.info, log: OSLog.tests, "%@", "\(response)")
-		})
-		return (expectations: [finished], cancellable: cancellable)
-	}
+            let data = Data("{}".utf8)
+            return (Response.valid, data)
+        }
+
+        let publisher = webService.dataPublisher(for: request)
+        let invalidTest = evalValidResponseTest(publisher: publisher)
+        wait(for: invalidTest.expectations, timeout: testTimeout)
+        invalidTest.cancellable?.cancel()
+    }
+
+    func testNetworkFailure() throws {
+        let request = URLRequest(url: Self.baseURL)
+            .setMethod(.get)
+        let requestURL = request.url
+        XCTAssertNotNil(requestURL)
+        URLProtocolMock.requestHandlers[requestURL!] = { request in
+            guard let url = request.url, url == requestURL else {
+                throw URLError(.badURL)
+            }
+
+            let data = Data("{}".utf8)
+            return (Response.invalid401, data)
+        }
+        let publisher = webService.dataPublisher(for: request)
+        let invalidTest = evalInvalidResponseTest(publisher: publisher)
+        wait(for: invalidTest.expectations, timeout: testTimeout)
+        invalidTest.cancellable?.cancel()
+    }
+
+    func evalValidResponseTest<P: Publisher>(publisher: P?) -> (expectations: [XCTestExpectation], cancellable: AnyCancellable?) {
+        XCTAssertNotNil(publisher)
+
+        let finished = expectation(description: "Finished")
+
+        let cancellable = publisher?.sink(receiveCompletion: { completion in
+            if case .failure(let error) = completion {
+                os_log(.error, log: OSLog.tests, "TEST ERROR %@", error.localizedDescription)
+            }
+            finished.fulfill()
+        }, receiveValue: { response in
+            XCTAssertNotNil(response)
+            os_log(.info, log: OSLog.tests, "%@", "\(response)")
+        })
+        return (expectations: [finished], cancellable: cancellable)
+    }
+
+    func evalInvalidResponseTest<P: Publisher>(publisher: P?) -> (expectations: [XCTestExpectation], cancellable: AnyCancellable?) {
+        XCTAssertNotNil(publisher)
+
+        let finished = expectation(description: "Finsihed")
+
+        let cancellable = publisher?.sink(receiveCompletion: { completion in
+            switch completion {
+            case .failure(let error):
+                os_log(.error, log: OSLog.tests, "TEST FULFILLED %@", error.localizedDescription)
+            case .finished:
+                XCTFail("Result should be error")
+            }
+            finished.fulfill()
+        }, receiveValue: { response in
+            XCTAssertNotNil(response)
+            os_log(.info, log: OSLog.tests, "%@", "\(response)")
+        })
+        return (expectations: [finished], cancellable: cancellable)
+    }
 }
 
 extension WebServiceTests {
-	func testAsync() async throws {
-		let request = URLRequest(url: Self.baseURL)
-		let requestURL = request.url
-		XCTAssertNotNil(requestURL)
-		URLProtocolMock.requestHandlers[requestURL!] = { request in
-			guard let url = request.url, url == requestURL else {
-				throw URLError(.badURL)
-			}
+    func testAsync() async throws {
+        let request = URLRequest(url: Self.baseURL)
+        let requestURL = request.url
+        XCTAssertNotNil(requestURL)
+        URLProtocolMock.requestHandlers[requestURL!] = { request in
+            guard let url = request.url, url == requestURL else {
+                throw URLError(.badURL)
+            }
 
-			return (Response.valid, Data())
-		}
+            return (Response.valid, Data())
+        }
 
-		let (data, _) = try await webService.data(for: request)
-		XCTAssertEqual(data, Data())
-	}
+        let (data, _) = try await webService.data(for: request)
+        XCTAssertEqual(data, Data())
+    }
 
-	func testDecodableData() throws {
-		let data = try Bundle.module.data(forResource: "Response", withExtension: "json", subdirectory: "TestData")
-		XCTAssertNotNil(data)
-		let decoded = try JSONDecoder().decode([String: String].self, from: data)
-		XCTAssertEqual(decoded.count, 2)
-		XCTAssertEqual(decoded["key1"], "value1")
-		XCTAssertEqual(decoded["key2"], "value2")
-		XCTAssertEqual(decoded["key3"], nil)
-	}
+    func testDecodableData() throws {
+        let data = try Bundle.module.data(forResource: "Response", withExtension: "json", subdirectory: "TestData")
+        XCTAssertNotNil(data)
+        let decoded = try JSONDecoder().decode([String: String].self, from: data)
+        XCTAssertEqual(decoded.count, 2)
+        XCTAssertEqual(decoded["key1"], "value1")
+        XCTAssertEqual(decoded["key2"], "value2")
+        XCTAssertEqual(decoded["key3"], nil)
+    }
 
-	func testAsyncDecodable() async throws {
-		let data = try Bundle.module.data(forResource: "Response", withExtension: "json", subdirectory: "TestData")
-		XCTAssertNotNil(data)
-		let request = URLRequest(url: Self.baseURL)
-			.setMethod(.get)
-		let requestURL = request.url
-		XCTAssertNotNil(requestURL)
-		URLProtocolMock.requestHandlers[requestURL!] = { request in
-			guard let url = request.url, url == requestURL else {
-				throw URLError(.badURL)
-			}
+    func testAsyncDecodable() async throws {
+        let data = try Bundle.module.data(forResource: "Response", withExtension: "json", subdirectory: "TestData")
+        XCTAssertNotNil(data)
+        let request = URLRequest(url: Self.baseURL)
+            .setMethod(.get)
+        let requestURL = request.url
+        XCTAssertNotNil(requestURL)
+        URLProtocolMock.requestHandlers[requestURL!] = { request in
+            guard let url = request.url, url == requestURL else {
+                throw URLError(.badURL)
+            }
 
-			return (Response.valid, data)
-		}
+            return (Response.valid, data)
+        }
 
-		let decoded: [String: String] = try await webService.decodable(for: request)
-		XCTAssertEqual(decoded.count, 2)
-		XCTAssertEqual(decoded["key1"], "value1")
-		XCTAssertEqual(decoded["key2"], "value2")
-		XCTAssertEqual(decoded["key3"], nil)
-	}
+        let decoded: [String: String] = try await webService.decodable(for: request)
+        XCTAssertEqual(decoded.count, 2)
+        XCTAssertEqual(decoded["key1"], "value1")
+        XCTAssertEqual(decoded["key2"], "value2")
+        XCTAssertEqual(decoded["key3"], nil)
+    }
 
-	func testAsyncSerializable() async throws {
-		let data = try Bundle.module.data(forResource: "Response", withExtension: "json", subdirectory: "TestData")
-		XCTAssertNotNil(data)
-		let request = URLRequest(url: Self.baseURL)
-			.setMethod(.get)
-		let requestURL = request.url
-		XCTAssertNotNil(requestURL)
-		URLProtocolMock.requestHandlers[requestURL!] = { request in
-			guard let url = request.url, url == requestURL else {
-				throw URLError(.badURL)
-			}
+    func testAsyncSerializable() async throws {
+        let data = try Bundle.module.data(forResource: "Response", withExtension: "json", subdirectory: "TestData")
+        XCTAssertNotNil(data)
+        let request = URLRequest(url: Self.baseURL)
+            .setMethod(.get)
+        let requestURL = request.url
+        XCTAssertNotNil(requestURL)
+        URLProtocolMock.requestHandlers[requestURL!] = { request in
+            guard let url = request.url, url == requestURL else {
+                throw URLError(.badURL)
+            }
 
-			return (Response.valid, data)
-		}
+            return (Response.valid, data)
+        }
 
-		let responseItem = try await webService.serializable(for: request)
-		let decoded = responseItem as? [String: String]
-		XCTAssertNotNil(decoded)
-		XCTAssertEqual(decoded!.count, 2)
-		XCTAssertEqual(decoded!["key1"], "value1")
-		XCTAssertEqual(decoded!["key2"], "value2")
-		XCTAssertEqual(decoded!["key3"], nil)
-	}
+        let responseItem = try await webService.serializable(for: request)
+        let decoded = responseItem as? [String: String]
+        XCTAssertNotNil(decoded)
+        XCTAssertEqual(decoded!.count, 2)
+        XCTAssertEqual(decoded!["key1"], "value1")
+        XCTAssertEqual(decoded!["key2"], "value2")
+        XCTAssertEqual(decoded!["key3"], nil)
+    }
 }
