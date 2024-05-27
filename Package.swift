4c2a9dbb9d206e8d03ec087e415a4af037cfe983
Waqar Malik <wmalloc@users.noreply.github.com>
2024-05-27 16:02:15 -0700

    Fixed the warnings regarding sendable

--- Package.swift
+++ Package.swift
@@ -2,16 +2,6 @@
 
 import PackageDescription
 
-let swiftSettings: [SwiftSetting] = [
-  .enableUpcomingFeature("BareSlashRegexLiterals"),
-  .enableUpcomingFeature("ConciseMagicFile"),
-  .enableUpcomingFeature("ExistentialAny"),
-  .enableUpcomingFeature("ForwardTrailingClosures"),
-  .enableUpcomingFeature("ImplicitOpenExistentials"),
-  .enableUpcomingFeature("StrictConcurrency"),
-  .unsafeFlags(["-warn-concurrency", "-enable-actor-data-race-checks"]),
-]
-
 let package = Package(
     name: "WebService",
     defaultLocalization: "en",
@@ -21,10 +11,10 @@ let package = Package(
         .library(name: "WebServiceURLMock", targets: ["WebServiceURLMock"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/wmalloc/HTTPRequestable.git", from: "0.7.0"),
+        .package(url: "https://github.com/wmalloc/HTTPRequestable.git", from: "0.7.2"),
     ],
     targets: [
-        .target(name: "WebService", dependencies: ["HTTPRequestable"], swiftSettings: swiftSettings),
+        .target(name: "WebService", dependencies: ["HTTPRequestable"], swiftSettings: []),
         .target(name: "WebServiceURLMock", dependencies: ["WebService"]),
         .testTarget(name: "WebServiceTests", dependencies: ["HTTPRequestable", "WebService", "WebServiceURLMock"],
                     resources: [.copy("TestData")]),
