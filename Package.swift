ec94e428c4f4274cc9cd674df95f115786b5c2c2
Waqar Malik <wmalloc@users.noreply.github.com>
2024-10-09 16:24:54 -0700

    Moved non async methods (#86)
    * Moved All the non async functions to webservice
    
    * Updated to be concurrency compliant

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.9
+// swift-tools-version:6.0
 
 import PackageDescription
 
@@ -7,16 +7,15 @@ let package = Package(
   defaultLocalization: "en",
   platforms: [.iOS(.v16), .tvOS(.v16), .macOS(.v12), .watchOS(.v9), .macCatalyst(.v16), .visionOS(.v1)],
   products: [
-    .library(name: "WebService", targets: ["WebService"]),
-    .library(name: "WebServiceURLMock", targets: ["WebServiceURLMock"])
+    .library(name: "WebService", targets: ["WebService"])
   ],
   dependencies: [
     .package(url: "https://github.com/wmalloc/HTTPRequestable.git", from: "0.8.0")
   ],
   targets: [
     .target(name: "WebService", dependencies: ["HTTPRequestable"], swiftSettings: []),
-    .target(name: "WebServiceURLMock", dependencies: ["WebService"]),
-    .testTarget(name: "WebServiceTests", dependencies: ["HTTPRequestable", "WebService", "WebServiceURLMock"],
+    .testTarget(name: "WebServiceTests", dependencies: ["HTTPRequestable", "WebService",
+                                                        .product(name: "MockURLProtocol", package: "HTTPRequestable")],
                 resources: [.copy("TestData")])
   ]
 )
